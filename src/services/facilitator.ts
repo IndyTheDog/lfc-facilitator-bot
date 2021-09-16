0cbc7d48200102579928497124e0a68769147158
Cédric Rochefolle <85271877+IndyTheDog@users.noreply.github.com>
2021-09-16 09:42:38 +0700

    Add default team support

--- src/services/facilitator.ts
+++ src/services/facilitator.ts
@@ -2,13 +2,29 @@ import { randomInt } from 'crypto';
 import { postSlackMessage } from '../repository/slack';
 
 export const choseFacilitator = async (requestor: string, facilitators: string[], channel: string) => {
-  const facilitator = facilitators[randomInt(facilitators.length)];
-  return postSlackMessage(
-    channel,
-    `:mega: ${requestor} requested a facilitator between [${facilitators.join(
-      ',',
-    )}].\n*${facilitator}* is the chosen one :tada:`,
-  ).then(() => {
-    return `Facilitator sent to ${channel} channel.`;
-  });
+
+    const getFacilitators = (inputFacilitators: string[]) => {
+        if (inputFacilitators
+            && inputFacilitators[0] === 'team'
+            && !!process.env.DEFAULT_TEAM) {
+            return process.env.DEFAULT_TEAM as unknown as string[];
+        }
+
+        return inputFacilitators;
+    }
+
+    const getFacilitator = (inputFacilitators: string[]) => {
+        return inputFacilitators[randomInt(inputFacilitators.length)];
+    }
+
+    const allFacilitators = getFacilitators(facilitators);
+    const facilitator = getFacilitator(allFacilitators);
+    return postSlackMessage(
+        channel,
+        `:mega: ${requestor} requested a facilitator between [${allFacilitators.join(
+            ',',
+        )}].\n*${facilitator}* is the chosen one :tada:`,
+    ).then(() => {
+        return `Facilitator sent to ${channel} channel.`;
+    });
 };
